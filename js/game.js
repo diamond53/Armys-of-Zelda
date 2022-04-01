0dc6c1cf0a9cb0e66c2e1018a5d0c5281a47fadb
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2022-04-01 17:10:11 +1100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,45 +1,44 @@
-window.Game = {};
-Game.saves = {'slot1':{}, 'slot2':{}, 'slot3':{}};
-Game.init = function() {
-	Game.setScreen("#start");
+window.Game = {}; // Initiate Game object
+Game.saves = {'slot1':{}, 'slot2':{}, 'slot3':{}}; // Initiate save structure
+Game.init = function() { // Function called when page start
+	Game.setScreen("#load");
 	Game.loadSave()
 	Game.main()
 }
-$(document).ready(function() {
+$(document).ready(function() { // jQuery code. Fires when page ready and responsive. Do not edit
 	Game.init()
 })
-Game.load = function(slot) {
+Game.load = function(slot) { // WIP. Save loading function
 	if (slot === 0) {
 		Game.setScreen("#saves")
 		return
 	}
 }
-Game.new = function(slot) {
+Game.new = function(slot) { // Creates a new save using Game.slot() object constructor
 	if (slot === 0) {
 		Game.setScreen("#saves")
 		return
 	}
 	Game.saves["slot"+slot] = new Game.slot()
-	Game.save()
 }
-Game.save = function() {
+Game.save = function() { // Save
 	localStorage.setItem('psim-save', btoa(JSON.stringify(Game.saves)));
 }
-Game.loadSave = function() {
+Game.loadSave = function() { // Load Save
 	if (localStorage.getItem('psim-save')) {
 		Game.saves = JSON.parse(atob(localStorage.getItem('psim-save')));
 	}
 }
-Game.setScreen = function(tab) {
+Game.setScreen = function(tab) { // Sets Screen. Usage: Game.setScreen("#[id attribute of <div> tag]")
 	$(".screen").removeClass('hidden visible');
 	$(".screen").addClass('hidden');
 	$(tab).addClass('visible');
 	$(tab).removeClass('hidden');
 }
-Game.slot = function() {
+Game.slot = function() { // Object constructor for new save
 	this.time = 0;
 }
-Game.main = function() {
+Game.main = function() { // Game loop function. Runs before each frame. 30fps = 30 runs a second. 60fps = 60 runs a second. Saves game.
 	window.requestAnimationFrame(Game.main);
 	Game.save()
 }
